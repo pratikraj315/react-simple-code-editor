a9d1f00f7f5e5816c03c81a97af3faac3405f59b
Pratik <pratikraj2401@gmail.com>
2024-07-01 11:19:08 +0530

    first commit

---
+++ src/CodeEditor.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Highlight, themes } from 'prism-react-renderer';
+import './CodeEditor.css';
+
+
+
+const CodeEditor = () => {
+  const defaultCode = `import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));`;
+
+  const [code, setCode] = useState(defaultCode);
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const handleGithubRedirect = () => {
+    window.location.href = 'https://www.github.com';
+  };
+
+  return (
+    <div className="code-editor-container">
+      <div className="editor-wrapper">
+        <Highlight
+          theme={themes.dracula}
+          code={code}
+          language="javascript"
+        >
+          {({ style, tokens, getLineProps, getTokenProps }) => (
+            <pre className="code-highlight" style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+        <textarea
+          className="code-input"
+          value={code}
+          onChange={handleChange}
+          spellCheck="false"
+        />
+      </div>
+      <button className="github-button" onClick={handleGithubRedirect}>
+        GitHub
+      </button>
+    </div>
+  );
+};
+
+export default CodeEditor;
